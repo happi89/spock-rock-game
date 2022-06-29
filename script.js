fc63f06350d8003f8b809724c8031f09e73192fb
happi89 <farhaanp99@gmail.com>
2022-06-29 00:19:07 -0400

    dom elements selected and added player selection

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
+const playerScore = document.getElementById('playerScore');
+const playerChoice = document.getElementById('playerChoice');
+const computerScore = document.getElementById('computerScore');
+const computerChoice = document.getElementById('computerChoice');
+const resultText = document.getElementById('resultText');
+const gameIcons = document.querySelectorAll('.far')
 
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
+
+// reset all selected icons 
+function resetSelected() {
+  gameIcons.forEach((icon) => {
+    icon.classList.remove('selected');
+  });
+}
+
+function select(choice) {
+  resetSelected();
+  // update player choice and add 'selected' class
+  switch(choice) {
+    case 'rock':
+      playerChoice.textContent = '--- Rock';
+      playerRock.classList.add('selected');
+      break;
+    case 'paper':
+      playerChoice.textContent = '--- Paper';
+      playerPaper.classList.add('selected');
+      break;
+    case 'scissors':
+      playerChoice.textContent = '--- Scissors';
+      playerScissors.classList.add('selected');
+      break;
+    case 'lizard':
+      playerChoice.textContent = '--- Lizard';
+      playerLizard.classList.add('selected');
+      break;
+    case 'spock':
+      playerChoice.textContent = '--- Spock';
+      playerSpock.classList.add('selected');
+      break;
+    default:
+      break;
+  }
+}
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
